34c7ee2600e0eeba3a11811969ea3aa366fa5515
Cara Eppes <caraeppes@zipcoders-MacBook-Pro-4.local>
2019-02-14 15:58:09 -0500

    completed Phonebook methods and tests

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,101 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private TreeMap<String, ArrayList<String>> phonebookMap;
+
+    public PhoneBook(){
+        this.phonebookMap = new TreeMap<String, ArrayList<String>>();
+    }
+
+    public TreeMap<String, ArrayList<String>> getPhonebookMap() {
+        return phonebookMap;
+    }
+
+    // adds an entry to the composite associate data type
+    public void add(String name, String phoneNumber){
+        if (phonebookMap.containsKey(name)){
+            phonebookMap.get(name).add(phoneNumber);
+        }
+        else {
+            ArrayList<String> numbers = new ArrayList<String>();
+            numbers.add(phoneNumber);
+            phonebookMap.put(name, numbers);
+        }
+    }
+
+    // removes an entry to the composite associate data type
+    public void remove(String name, String phoneNumber){
+        try{
+            if (phonebookMap.get(name).size() == 1){
+                this.removeRecord(name);
+            }
+            else {
+                phonebookMap.get(name).remove(phoneNumber);
+            }
+        }
+        catch(NullPointerException e){
+            System.out.println("Entry does not exist in this Phonebook");
+        }
+    }
+
+    // returns a phone number for the respective input name
+    public ArrayList<String> lookup(String name){
+        return phonebookMap.get(name);
+    }
+
+    // returns a name for the respective input phoneNumber
+    public String reverseLookup(String phoneNumber){
+        for (Map.Entry<String, ArrayList<String>> entry : phonebookMap.entrySet()){
+            if (entry.getValue().contains(phoneNumber)){
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    // print a human-readable list of all entries (names and phone numbers) in alphabetical order.
+    public void display(){
+        for (Map.Entry<String, ArrayList<String>> entry : phonebookMap.entrySet()){
+            System.out.println(entry.getKey() + ": ");
+            for (String number : entry.getValue()){
+                System.out.println(number);
+            }
+        }
+    }
+
+    // removeRecord method for removing an entire entry from your PhoneBook.
+    public void removeRecord(String name){
+        phonebookMap.remove(name);
+    }
+
+
+   /*
+   public static void main(String[] args) {
+
+        PhoneBook phonebook = new PhoneBook();
+        TreeMap<String, ArrayList<String>> map = phonebook.getPhonebookMap();
+        String name = "Cara";
+        String phonenumber = "3027507192";
+        String phonenumber2 = "3024786620";
+        phonebook.add(name, phonenumber);
+        phonebook.add(name, phonenumber2);
+        String anne = "Anne";
+        phonebook.add(anne, "302747939");
+        String bob = "Bob";
+        phonebook.add(bob, phonenumber2);
+        phonebook.add(bob, phonenumber);
+        phonebook.display();
+
+        phonebook.remove("fsd", "");
+    }
+    */
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,122 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void addTest(){
+        // Given
+        PhoneBook phonebook = new PhoneBook();
+        TreeMap<String, ArrayList<String>> map = phonebook.getPhonebookMap();
+        String name = "Cara";
+        String phonenumber = "3027507192";
+        String phonenumber2 = "3024786620";
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add(phonenumber);
+
+        // When
+        phonebook.add(name, phonenumber);
+        ArrayList<String> actual = map.get(name);
+
+        // Then
+        Assert.assertEquals(phonenumber, map.get(name).get(0));
+        Assert.assertEquals(expected, actual);
+        Assert.assertNull(map.get("notakey"));
+
+        // When
+        phonebook.add(name, phonenumber2);
+        expected.add(phonenumber2);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(phonenumber2, map.get(name).get(1));
+    }
+
+    @Test
+    public void removeTest(){
+        // Given
+        PhoneBook phonebook = new PhoneBook();
+        TreeMap<String, ArrayList<String>> map = phonebook.getPhonebookMap();
+        String name = "Cara";
+        String phonenumber = "3027507192";
+        String phonenumber2 = "3024786620";
+        phonebook.add(name, phonenumber);
+        phonebook.add(name, phonenumber2);
+
+        // When
+        phonebook.remove(name, phonenumber);
+
+        // Then
+        Assert.assertEquals(phonenumber2, map.get(name).get(0));
+
+        // When
+        phonebook.remove(name, phonenumber2);
+
+        // Then
+        Assert.assertNull(map.get(name));
+        Assert.assertEquals(0, map.size());
+
+    }
+
+    @Test
+    public void lookupTest(){
+        // Given
+        PhoneBook phonebook = new PhoneBook();
+        TreeMap<String, ArrayList<String>> map = phonebook.getPhonebookMap();
+        String name = "Cara";
+        String phonenumber = "3027507192";
+        phonebook.add(name, phonenumber);
+
+        // When
+        ArrayList<String> actual = phonebook.lookup(name);
+
+        // Then
+        Assert.assertEquals(phonenumber, actual.get(0));
+    }
+
+    @Test
+    public void reverseLookupTest(){
+        // Given
+        PhoneBook phonebook = new PhoneBook();
+        TreeMap<String, ArrayList<String>> map = phonebook.getPhonebookMap();
+        String name = "Cara";
+        String phonenumber = "3027507192";
+        String phonenumber2 = "3024786620";
+        phonebook.add(name, phonenumber);
+        phonebook.add(name, phonenumber2);
+
+        // When
+        String actual = phonebook.reverseLookup(phonenumber);
+        String actual2 = phonebook.reverseLookup(phonenumber2);
+
+        // Then
+        Assert.assertEquals(name, actual);
+        Assert.assertEquals(name, actual2);
+        Assert.assertNull(phonebook.reverseLookup("notaphonenumer"));
+    }
+
+    @Test
+    public void removeRecordTest(){
+        // Given
+        PhoneBook phonebook = new PhoneBook();
+        TreeMap<String, ArrayList<String>> map = phonebook.getPhonebookMap();
+        String name = "Cara";
+        String phonenumber = "3027507192";
+        phonebook.add(name, phonenumber);
+
+        // When
+        phonebook.removeRecord(name);
+
+        // Then
+        Assert.assertNull(map.get(name));
+        Assert.assertEquals(0, map.size());
+
+    }
 }
